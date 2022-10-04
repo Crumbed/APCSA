14d3dc7dbb373666b3461dca614c92de9eccbdb2
Crumbed <kaiharrelson5466@gmail.com>
2022-10-04 09:13:08 -0400

    10/4/22

---
+++ Unit2/src/testing/Testing.java
@@ -0,0 +1,13 @@
+package testing;
+
+public class Testing {
+
+    public static void main(String[] args) {
+        
+        String s1 = "ac ded ca";
+        int pos = s1.indexOf("d");
+
+        System.out.println(pos);
+    }
+    
+}
