9f7292acd559473c2e43516fba05e72f6458a8a2
Crumbed <kaiharrelson5466@gmail.com>
2022-10-24 08:42:08 -0400

    asdf

---
+++ Unit3/src/PogilBoolean/Main.java
@@ -0,0 +1,18 @@
+package PogilBoolean;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 3, b = 4, c= 5;
+
+        System.out.println(!(a > c));  // p
+        System.out.println(!a > c);  // q
+
+
+        
+        
+
+
+    }
+    
+
+}
